438a6e688f469a8f802b2b333b7cefece6fbd141
bendude56 <bendude56@gmail.com>
2015-08-11 12:27:15 -0600

    Show the version number in the footer

--- OpenAnnounce/Config.cs
+++ OpenAnnounce/Config.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace OpenAnnounce
@@ -9,5 +10,7 @@ namespace OpenAnnounce
     public static class Config
     {
         public static string Title { get { return ConfigurationManager.AppSettings["PageTitle"]; } }
+
+        public static Assembly WebAssembly { get { return Assembly.GetExecutingAssembly(); } }
     }
 }
